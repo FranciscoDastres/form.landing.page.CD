91404a5f7e533e995f91f8a78841034183b5e548
Francisco Meza Dastres <franciscomeza.1234@gmail.com>
2024-08-30 22:35:29 -0400

    cambiando estilos y modificando index

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,16 @@ handleSelectChange = (selectId, specifyDivId, otherInputId) => {
 }
 handleSelectChange('tipoProyecto', 'projectSpecify', 'projectOther');
 handleSelectChange('howFindUs', 'findUsSpecify', 'findUsOther');
+
+
+document.getElementById('contactForm').addEventListener('submit',  (event) =>{
+  event.preventDefault(); // Previene el envío normal del formulario
+  $('#contactModal').modal('show'); // Muestra el modal
+
+  // Reinicia el formulario
+  document.getElementById('contactForm').reset();
+
+  // Opcional: Ocultar campos adicionales si es necesario
+  document.getElementById('projectSpecify').style.display = 'none';
+  document.getElementById('findUsSpecify').style.display = 'none';
+});
