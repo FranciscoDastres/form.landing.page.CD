39001c8d93ab883e4bb50b69b5f115a8904ffb68
Francisco Meza Dastres <franciscomeza.1234@gmail.com>
2024-08-30 21:21:25 -0400

    modify script function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,8 @@
-// document.getElementById('tipoProyecto').addEventListener('change', (event) => {
-//   const especificarOtra = document.getElementById('especificarOtra');
-//   const otraEspecificarInput = document.getElementById('otraEspecificar');
-  
-//   if (event.target.value === 'otra') {
-//     especificarOtra.style.display = 'block';
-//     otraEspecificarInput.required = true; // Hacer el campo obligatorio
-//   } else {
-//     especificarOtra.style.display = 'none';
-//     otraEspecificarInput.required = false; // Quitar la obligatoriedad si no está seleccionado
-//     otraEspecificarInput.value = ''; // Limpiar el campo si se oculta
-//   }
-// });
-
-function handleSelectChange(selectId, specifyDivId, otherInputId) {
+handleSelectChange = (selectId, specifyDivId, otherInputId) => {
   const selectElement = document.getElementById(selectId);
   const specifyDiv = document.getElementById(specifyDivId);
   const otherInput = document.getElementById(otherInputId);
-  
+
   selectElement.addEventListener('change', (event) => {
     if (event.target.value === 'otra') {
       specifyDiv.style.display = 'block';
@@ -28,7 +14,5 @@ function handleSelectChange(selectId, specifyDivId, otherInputId) {
     }
   });
 }
-
-// Aplicar la misma lógica para ambos selectores
 handleSelectChange('tipoProyecto', 'projectSpecify', 'projectOther');
 handleSelectChange('howFindUs', 'findUsSpecify', 'findUsOther');
